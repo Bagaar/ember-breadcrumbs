47650db392e0df85f0904a8f1adb45f9dff1d5b6
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2019-03-21 13:45:04 +0100

    chore: Add `ember-angle-bracket-invocation-polyfill` to ember-try
    Ember apps below 3.4 need the Angle Bracket Invocation polyfill for the addon to work

--- config/ember-try.js
+++ config/ember-try.js
@@ -21,6 +21,7 @@ module.exports = function () {
           npm: {
             devDependencies: {
               '@ember/jquery': '^0.5.1',
+              'ember-angle-bracket-invocation-polyfill': '^1.2.5',
               'ember-source': '~2.16.0'
             }
           }
@@ -35,6 +36,7 @@ module.exports = function () {
           npm: {
             devDependencies: {
               '@ember/jquery': '^0.5.1',
+              'ember-angle-bracket-invocation-polyfill': '^1.2.5',
               'ember-source': '~2.18.0'
             }
           }
