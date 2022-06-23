f4d234716738a69caa740849292d8169835e0c93
Bert De Block <bert.deblock@bagaar.be>
2022-06-23 13:13:12 +0200

    test: update import paths

--- tests/integration/components/breadcrumbs-container-test.js
+++ tests/integration/components/breadcrumbs-container-test.js
@@ -1,5 +1,5 @@
 import { render } from '@ember/test-helpers';
-import { setupRenderingTest } from 'ember-qunit';
+import { setupRenderingTest } from 'dummy/tests/helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
 
